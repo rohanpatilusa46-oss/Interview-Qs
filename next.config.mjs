85ca438162fcc7ea59669c4b7fa7cf16e4e37913
Rohan Patil <rohanpatil.usa46@gmail.com>
2026-04-05 19:39:54 -0400

    added redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/projects/interview',
+        destination: 'https://interviewqs-seven.vercel.app',
+        permanent: true,
+      },
+      {
+        source: '/projects/interview/:path*',
+        destination: 'https://interviewqs-seven.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
